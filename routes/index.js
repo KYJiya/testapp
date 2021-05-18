e12cd7016ff308f4bd9522a09416567f5d68bd1e
signtquater <sightquater@naver.com>
2021-05-18 11:04:31 +0900

    first commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+const userController = require('../controllers');
+
+/* GET home page. */
+router.get('/', userController.basicAPI);
+router.get('/test', userController.testAPI);
+router.post('/post_test', userController.postTestAPI);
+
+module.exports = router;
